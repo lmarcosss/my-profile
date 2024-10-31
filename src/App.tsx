b9165b10c9361b270eec6e8ab2ff4098f292455f
Leonardo dos Santos Marcos <leonardo.marcos@cwi.com.br>
2024-10-31 01:48:20 -0300

    Include tailwind and frame motion

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,32 @@
+import { motion } from "framer-motion";
+
 import './App.css'
-import assets from './assets'
+
+import profileImage from './assets/profile-image.jpeg'
+
+// const profileImage = "https://firebasestorage.googleapis.com/v0/b/my-profile-afdb3.appspot.com/o/profile-image.jpeg?alt=media&token=6bc1522b-4ac5-470b-977c-5053d811eed9"
 
 export default function App() {
   return (
     <div className='container'>
-      <img className='profile-image' src={assets.profileImage} />
-      <h1>Hi, I'm Leo. <br/>I'm a software developer.</h1>
+      <motion.img
+        initial={{ scale: 0 }}
+        animate={{ scale: 1 }}
+        transition={{
+          type: "spring",
+        }}
+        className='profile-image' src={profileImage}
+      />
+        {/* <img className='profile-image' src={profileImage} /> */}
+      {/* </motion.image> */}
+      <motion.h1
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{
+          delay: 0.5,
+          duration: 2,
+        }}
+      >Hi, I'm Leo. <br/>I'm a software developer.</motion.h1>
     </div>
   )
 }
--- src/assets/index.ts
+++
@@ -1,5 +0,0 @@
-import profileImage from './profile-image.jpeg'
-
-export default {
-  profileImage
-}
