b0637995eadb84e93d067fd82fb78e85a400c208
Leonardo dos Santos Marcos <leonardo.marcos@cwi.com.br>
2024-10-31 01:10:13 -0300

    Include image of me in content

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import './App.css'
+import assets from './assets'
 
 export default function App() {
   return (
-    <h1>Hi, I'm Leo. I'm a software developer.</h1>
+    <div className='container'>
+      <img className='profile-image' src={assets.profileImage} />
+      <h1>Hi, I'm Leo. <br/>I'm a software developer.</h1>
+    </div>
   )
 }
---
+++ src/assets/index.ts
@@ -0,0 +1,5 @@
+import profileImage from './profile-image.jpeg'
+
+export default {
+  profileImage
+}
