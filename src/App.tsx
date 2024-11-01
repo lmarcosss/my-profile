0a714b9c25edfa8994b055f0d8a746ef5b09aae9
Leonardo dos Santos Marcos <leonardo.marcos@cwi.com.br>
2024-11-01 01:31:23 -0300

    Modify prettier

--- src/App.tsx
+++ src/App.tsx
@@ -7,22 +7,22 @@ import { TypingAnimation } from './components/typing-animation'
 import './App.css'
 
 export default function App() {
-    return (
-        <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-            <div className="container">
-                <motion.img
-                    initial={{ scale: 0 }}
-                    animate={{ scale: 1 }}
-                    transition={{
-                        type: 'spring',
-                    }}
-                    className="profile-image"
-                    src={profileImage}
-                />
-                <TypingAnimation text="Hi, I'm Leo. I'm a software developer." />
+  return (
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <div className="container">
+        <motion.img
+          initial={{ scale: 0 }}
+          animate={{ scale: 1 }}
+          transition={{
+            type: 'spring',
+          }}
+          className="profile-image"
+          src={profileImage}
+        />
+        <TypingAnimation text="Hi, I'm Leo. I'm a software developer." />
 
-                <ModeToggle />
-            </div>
-        </ThemeProvider>
-    )
+        <ModeToggle />
+      </div>
+    </ThemeProvider>
+  )
 }
--- src/components/typing-animation.tsx
+++ src/components/typing-animation.tsx
@@ -2,43 +2,43 @@ import { motion } from 'framer-motion'
 import { useEffect, useState } from 'react'
 
 interface TypingAnimationProps {
-    text: string
+  text: string
 }
 
 export function TypingAnimation({ text }: TypingAnimationProps) {
-    const [displayedText, setDisplayedText] = useState('')
-    const [index, setIndex] = useState(0)
-    const typingSpeed = 100
+  const [displayedText, setDisplayedText] = useState('')
+  const [index, setIndex] = useState(0)
+  const typingSpeed = 100
 
-    useEffect(() => {
-        const typingInterval = setInterval(() => {
-            if (index < text.length) {
-                setDisplayedText((prev) => prev + text.charAt(index))
+  useEffect(() => {
+    const typingInterval = setInterval(() => {
+      if (index < text.length) {
+        setDisplayedText((prev) => prev + text.charAt(index))
 
-                setIndex((prev) => prev + 1)
-            } else {
-                clearInterval(typingInterval)
-            }
-        }, typingSpeed)
+        setIndex((prev) => prev + 1)
+      } else {
+        clearInterval(typingInterval)
+      }
+    }, typingSpeed)
 
-        return () => clearInterval(typingInterval)
-    }, [index, text])
+    return () => clearInterval(typingInterval)
+  }, [index, text])
 
-    return (
-        <div className="w-72">
-            <span className="text-3xl font-bold">
-                {displayedText}
-                <motion.span
-                    animate={{ opacity: [0, 1] }}
-                    transition={{
-                        duration: 1,
-                        repeat: Infinity,
-                        ease: 'easeInOut',
-                    }}
-                >
-                    |
-                </motion.span>
-            </span>
-        </div>
-    )
+  return (
+    <div className="w-72">
+      <span className="text-3xl font-bold">
+        {displayedText}
+        <motion.span
+          animate={{ opacity: [0, 1] }}
+          transition={{
+            duration: 1,
+            repeat: Infinity,
+            ease: 'easeInOut',
+          }}
+        >
+          |
+        </motion.span>
+      </span>
+    </div>
+  )
 }
