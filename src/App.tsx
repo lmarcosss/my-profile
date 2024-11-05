76cdebb5468a1628c8b2de947b3c0493b5648aa8
Leonardo dos Santos Marcos <leonardo.marcos@cwi.com.br>
2024-11-05 15:05:04 -0300

    Include easter egg on profile image

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,15 @@ import { ThemeProvider } from './contexts/theme-provider'
 import { ModeToggle } from './components/mode-toggle'
 import { TypingAnimation } from './components/typing-animation'
 import { Header } from './components/header'
+import { AnimatedCard } from './components/animated-card'
 
-import './App.css'
 import {
   GitHubLogoIcon,
   InstagramLogoIcon,
   LinkedInLogoIcon,
 } from '@radix-ui/react-icons'
+import { MailIcon } from 'lucide-react'
+import './App.css'
 
 const urls = [
   {
@@ -28,6 +30,11 @@ const urls = [
     icon: InstagramLogoIcon,
     hover: 'hover:text-red-400',
   },
+  {
+    url: 'mailto: marcosleonardosss@gmail.com',
+    icon: MailIcon,
+    hover: 'hover:text-cyan-400',
+  },
 ]
 
 export default function App() {
@@ -35,31 +42,32 @@ export default function App() {
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <Header />
 
-      <div className="w-full h-full flex flex-col justify-center items-center">
-        <div className="container">
-          <div className="flex flex-col items-center justify-center">
-            <motion.img
-              initial={{ scale: 0 }}
-              animate={{ scale: 1 }}
-              transition={{
-                type: 'spring',
-              }}
-              className="w-48 h-48 object-cover rounded-full border-4 dark:border-black border-white ring-emerald-500 ring-4 ring-none"
-              src={profileImage}
-              style={{
-                imageRendering: 'crisp-edges',
-              }}
-            />
-            <div className="flex flex-row gap-8 p-8 items-center justify-center w-72">
-              {urls.map(({ url, icon: Icon, hover }) => (
-                <a key={url} href={url} target="_blank">
-                  <Icon className={`size-7 ${hover}`} />
-                </a>
-              ))}
-            </div>
-          </div>
-          <TypingAnimation text="Hi, I'm Leo. I'm a software developer." />
+      <div className="w-full h-full flex flex-col justify-start items-center p-16">
+        <AnimatedCard>
+          <motion.img
+            initial={{ scale: 0 }}
+            animate={{ scale: 1 }}
+            transition={{
+              type: 'spring',
+            }}
+            className="w-48 h-48 object-cover rounded-full border-4 border-transparent
+              ring-emerald-500 ring-4 ring-none"
+            src={profileImage}
+            style={{
+              imageRendering: 'crisp-edges',
+            }}
+          />
+        </AnimatedCard>
+
+        <div className="flex gap-8 p-8 items-center justify-center w-72">
+          {urls.map(({ url, icon: Icon, hover }) => (
+            <a key={url} href={url} target="_blank">
+              <Icon className={`size-7 ${hover}`} />
+            </a>
+          ))}
         </div>
+
+        <TypingAnimation text="Hi, I'm Leo. I'm a software developer." />
       </div>
 
       <ModeToggle />
---
+++ src/components/animated-card.tsx
@@ -0,0 +1,55 @@
+import { useState } from 'react'
+import { motion } from 'framer-motion'
+
+export const AnimatedCard = ({
+  children,
+}: {
+  children: JSX.Element
+}) => {
+  const [flipped, setFlipped] = useState(false)
+
+  const onToggleFlip = () => {
+    setFlipped(!flipped)
+  }
+
+  return (
+    <div className="flex items-center justify-center flex-row">
+      <motion.div
+        className="relative w-48 h-48 cursor-pointer"
+        onClick={onToggleFlip}
+      >
+        <motion.div
+          className={`absolute inset-0 flex items-center justify-center rounded-lg ${
+            flipped ? 'rotate-y-180' : '' }`}
+          animate={{ rotateY: flipped ? 180 : 0 }}
+          transition={{ duration: 0.6 }}
+          style={{ backfaceVisibility: 'hidden', perspective: 1000 }}
+        >
+          {children}
+        </motion.div>
+
+        <motion.div
+          className={`absolute inset-0 flex items-center justify-center rounded-full
+            bg-slate-950 text-white ${flipped ? '' : 'rotate-y-180'} p-4 border-4
+            border-transparent ring-lime-400 ring-4 ring-none`}
+          animate={{ rotateY: flipped ? 0 : -180 }}
+          transition={{ duration: 0.6 }}
+          initial={false}
+          style={{ backfaceVisibility: 'hidden', perspective: 1000 }}
+        >
+          <span className="font-semibold">
+            You finded the easteregg!
+            <br />
+            <a
+              className="underline text-lime-400"
+              target="_blank"
+              href="https://www.youtube.com/watch?v=oowBXzfcl90"
+            >
+              Follow my lead
+            </a>
+          </span>
+        </motion.div>
+      </motion.div>
+    </div>
+  )
+}
