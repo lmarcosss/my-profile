bab96537014570b983d89381086dae5220e1bde7
Leonardo dos Santos Marcos <leonardo.marcos@cwi.com.br>
2024-11-06 15:24:40 -0300

    Include color of theme toggle button

--- src/components/mode-toggle.tsx
+++ src/components/mode-toggle.tsx
@@ -15,9 +15,19 @@ export function ModeToggle() {
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button className="fixed right-4 top-4" variant="outline" size="icon">
-          <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-          <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+        <Button
+          className="fixed right-4 top-4 bg-yellow-300"
+          variant="outline"
+          size="icon"
+        >
+          <Sun
+            className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all
+              dark:-rotate-90 dark:scale-0 text-black"
+          />
+          <Moon
+            className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all
+              dark:rotate-0 dark:scale-100 text-black"
+          />
           <span className="sr-only">Toggle theme</span>
         </Button>
       </DropdownMenuTrigger>
