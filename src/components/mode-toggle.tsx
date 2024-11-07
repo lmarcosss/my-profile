c8e23578a0d454234dd4973ae3f6cd88020465b5
Leonardo dos Santos Marcos <leonardo.marcos@cwi.com.br>
2024-11-07 02:19:00 -0300

    Modify shadow of toggle theme button color and size

--- src/components/mode-toggle.tsx
+++ src/components/mode-toggle.tsx
@@ -17,15 +17,15 @@ export function ModeToggle() {
       <DropdownMenuTrigger asChild>
         <Button
           className={`fixed right-4 top-4
-            ${theme === 'light' ? 'bg-yellow-300 hover:bg-yellow-200' : 'shadow-yellow-200 shadow '}
-            hover:border-none hover:ring-none hover:outline-none `}
+            ${theme === 'light' ? 'bg-yellow-300 hover:bg-yellow-200' : 'shadow-yellow-300 shadow-md '}
+            hover:border-none hover:ring-none hover:outline-none`}
           variant="outline"
           size="icon"
         >
           <Sun
             fill={'yellow'}
             className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all
-              dark:-rotate-90 dark:scale-0 text-black"
+              dark:-rotate-90 dark:scale-0 text-yellow-700"
           />
           <Moon
             fill={'yellow'}
