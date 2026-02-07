a414f4f1e17c9edaeee8b5cf1685fe8b6e67cb13
Leonardo dos Santos Marcos <leonardo.marcos@cwi.com.br>
2026-02-07 11:05:49 -0300

    Modify default font family of the project

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,9 @@ module.exports = {
   content: ['./index.html', './src/**/*.{ts,tsx,js,jsx}'],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ['Excalifont', 'Inter', 'system-ui', 'Avenir', 'Helvetica', 'Arial', 'sans-serif'],
+      },
       borderRadius: {
         lg: 'var(--radius)',
         md: 'calc(var(--radius) - 2px)',
